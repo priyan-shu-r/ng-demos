689812aa4f220adbd3207935419ce881512d1554
priyan-shu-r <priyanshu.r@codesolutions.com>
2022-02-11 19:16:27 +0530

    User Registration form

---
+++ Registration Form/form/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BasicInformationComponent } from './basic-information/basic-information.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BasicInformationComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
